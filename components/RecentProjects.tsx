0d9f680ee3174424fb9a30453c93b4838a8e94f0
Jitesh Dubey <jiteshdubey001@gmail.com>
2024-06-22 20:20:14 +0530

    Resolved Navbar Issues

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import React from "react";
 import { FaLocationArrow } from "react-icons/fa6";
 
 import { projects } from "@/data";
@@ -7,7 +7,7 @@ import { PinContainer } from "./ui/Pin";
 
 const RecentProjects = () => {
   return (
-    <div className="py-20">
+    <div className="py-20" id="projects">
       <h1 className="heading">
         A small selection of{" "}
         <span className="text-purple">recent projects</span>
